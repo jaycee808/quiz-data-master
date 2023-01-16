caeb9a0940b20115e22647f366abd662f8ebaa72
jaycee808 <jamie.clark@live.co.uk>
2023-01-16 18:14:37 +0000

    added function in javascript to display questions

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -42,16 +42,24 @@ var quizQuestions = [
 ];
 
 var timerEl = document.getElementById('time');
-var startQuiz = document.getElementById('start');
+var startBtn = document.getElementById('start');
 var quiz = document.getElementById('quiz');
 var questionTitle = document.getElementById('question-title');
 var questions = document.getElementById('questions');
 var choices = document.getElementById('choices');
+var choiceA = document.getElementById('choice-a');
+var choiceB = document.getElementById('choice-b');
+var choiceC = document.getElementById('choice-c');
+var choiceD = document.getElementById('choice-d');
 var submitBtn = document.getElementById('submit');
 var startScreen = document.getElementById('start-screen');
 
+var questionIndex = 0;
+var score = 0;
 
-startQuiz.addEventListener('click',
+questions.style.display = "none";
+
+
 function time() {
   var timeLeft = 75;
   var timeInterval = setInterval(function () {
@@ -61,5 +69,22 @@ function time() {
       if(timeLeft === 0) {
           clearInterval(timeInterval);
   }
-}, 1000)},
-);
+}, 1000)}
+
+function startQuestions() {
+  startScreen.style.display = "none";
+  questions.style.display = "block";
+  time();
+  questionDisplay();
+}
+
+function questionDisplay() {
+  questionTitle.textContent = quizQuestions[questionIndex].question;
+  // choices.innerHTML = "";
+  choiceA.textContent = quizQuestions[questionIndex].a;
+  choiceB.textContent = quizQuestions[questionIndex].b;
+  choiceC.textContent = quizQuestions[questionIndex].c;
+  choiceD.textContent = quizQuestions[questionIndex].d;
+}
+
+startBtn.onclick = startQuestions;
